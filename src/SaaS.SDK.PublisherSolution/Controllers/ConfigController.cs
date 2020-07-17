46733a5949d8a95e5cb17c3ef78dd82311563fd6
Victor Milovanov <lost@losttech.software>
2020-07-16 18:40:59 -0700

    added admin /Config to view application configuration

---
+++ src/SaaS.SDK.PublisherSolution/Controllers/ConfigController.cs
@@ -0,0 +1,33 @@
+﻿namespace SaaS.SDK.PublisherSolution.Controllers {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Marketplace.SaaS.SDK.Services.Utilities;
+    using Microsoft.Marketplace.Saas.Web.Controllers;
+    using Microsoft.Marketplace.SaasKit.Client.DataAccess.Contracts;
+
+    [ServiceFilter(typeof(KnownUserAttribute))]
+    public class ConfigController : BaseController {
+        readonly IApplicationConfigRepository configRepository;
+        readonly ILogger<ConfigController> logger;
+        public ConfigController(IApplicationConfigRepository configRepository, ILogger<ConfigController> logger) {
+            this.configRepository = configRepository;
+            this.logger = logger;
+        }
+
+        public IActionResult Index() {
+            this.logger.LogInformation("Config Controller / Index");
+            try {
+                var configSettings = this.configRepository.GetAll().ToList();
+                return this.View(configSettings);
+            } catch (Exception ex) {
+                this.logger.LogError(ex, ex.Message);
+                return this.View("Error", ex);
+            }
+        }
+    }
+}
